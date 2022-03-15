a8357eccfc76eb73297af59f82efa0c05fcb348f
Manikandan <mani.k@squashapps.com>
2022-03-15 10:48:33 +0530

    new file needs to change

---
+++ server.mjs
@@ -0,0 +1,16 @@
+import { createServer } from 'http'
+
+const port = 3001
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello!')
+}
+
+const server = createServer(requestHandler)
+server.listen(port, (err) => {
+  if(err){
+    return console.log('Something went wrong', err)
+  }
+  console.log('Server is Listening on ${port}')
+})
